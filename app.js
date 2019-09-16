f0ca9d08db919e1efc988f0099c86a669775013d
hongdanyang <hongdanyang1991@163.com>
2019-09-16 16:39:11 +0800

    修复沉浸式导航bug

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
+
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let custom = wx.getMenuButtonBoundingClientRect();
+        this.globalData.Custom = custom;
+        this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+      }
+    })
+
     // 登录
     wx.login({
       success: res => {
