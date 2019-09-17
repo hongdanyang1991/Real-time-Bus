e54da924f49c0b5d891b6ae96c0a19b8f80a3764
hongdanyang <hongdanyang1991@163.com>
2019-09-17 19:31:39 +0800

    统一绿色导航 & 删除多余页面

--- pages/bus/home/home.js
+++ pages/bus/home/home.js
@@ -7,37 +7,37 @@ Component({
       {
         title: '养老保险',
         name: 'list',
-        color: 'cyan',
+        color: 'green',
         icon: 'newsfill'
       },
       {
         title: '住房公积金',
         name: 'Accumulation fund',
-        color: 'cyan',
+        color: 'green',
         icon: 'newsfill'
       },
       {
         title: '医疗保险',
         name: 'car',
-        color: 'cyan',
+        color: 'green',
         icon: 'newsfill'
       },
       {
         title: '失业保险',
         name: 'car',
-        color: 'cyan',
+        color: 'green',
         icon: 'newsfill'
       },
       {
         title: '工伤保险',
         name: 'car',
-        color: 'cyan',
+        color: 'green',
         icon: 'newsfill'
       },
       {
         title: '生育保险',
         name: 'car',
-        color: 'cyan',
+        color: 'green',
         icon: 'newsfill'
       }
     ],
