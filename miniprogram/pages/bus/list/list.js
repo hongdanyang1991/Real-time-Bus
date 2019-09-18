73e92fb2d5ab7dbfbf24264cc8a125765020078e
hongdanyang <hongdanyang1991@163.com>
2019-09-18 16:41:17 +0800

    调通云数据库

--- miniprogram/pages/bus/list/list.js
+++ miniprogram/pages/bus/list/list.js
@@ -1,17 +1,29 @@
 const app = getApp();
 
 
+
 Page({
 
-  onQuery: function () {
+
+  data: {
+    book_list: []
+  },
+
+
+  /**
+  * 生命周期函数--监听页面加载
+  */
+  onLoad: function (options) {
+    var _this = this;
+
     const db = wx.cloud.database()
     // 查询当前用户所有的 counters
-    db.collection('counters').where({
+    db.collection('gjj-list').where({
       _openid: this.data.openid
     }).get({
       success: res => {
         this.setData({
-          queryResult: JSON.stringify(res.data, null, 2)
+          book_list: res.data
         })
         console.log('[数据库] [查询记录] 成功: ', res)
       },
@@ -23,135 +35,6 @@ Page({
         console.error('[数据库] [查询记录] 失败：', err)
       }
     })
-  },
-  
+  } 
 
-  data: {
-    StatusBar: app.globalData.StatusBar,
-    CustomBar: app.globalData.CustomBar,
-    Custom: app.globalData.Custom,
-    iconList: [{
-      icon: 'cardboardfill',
-      color: 'red',
-      badge: 120,
-      name: 'VR'
-    }, {
-      icon: 'recordfill',
-      color: 'orange',
-      badge: 1,
-      name: '录像'
-    }, {
-      icon: 'picfill',
-      color: 'yellow',
-      badge: 0,
-      name: '图像'
-    }, {
-      icon: 'noticefill',
-      color: 'olive',
-      badge: 22,
-      name: '通知'
-    }, {
-      icon: 'upstagefill',
-      color: 'cyan',
-      badge: 0,
-      name: '排行榜'
-    }, {
-      icon: 'clothesfill',
-      color: 'blue',
-      badge: 0,
-      name: '皮肤'
-    }, {
-      icon: 'discoverfill',
-      color: 'purple',
-      badge: 0,
-      name: '发现'
-    }, {
-      icon: 'questionfill',
-      color: 'mauve',
-      badge: 0,
-      name: '帮助'
-    }, {
-      icon: 'commandfill',
-      color: 'purple',
-      badge: 0,
-      name: '问答'
-    }, {
-      icon: 'brandfill',
-      color: 'mauve',
-      badge: 0,
-      name: '版权'
-    }],
-    gridCol: 3,
-    skin: false
-  },
-  showModal(e) {
-    this.setData({
-      modalName: e.currentTarget.dataset.target
-    })
-  },
-  hideModal(e) {
-    this.setData({
-      modalName: null
-    })
-  },
-  gridchange: function (e) {
-    this.setData({
-      gridCol: e.detail.value
-    });
-  },
-  gridswitch: function (e) {
-    this.setData({
-      gridBorder: e.detail.value
-    });
-  },
-  menuBorder: function (e) {
-    this.setData({
-      menuBorder: e.detail.value
-    });
-  },
-  menuArrow: function (e) {
-    this.setData({
-      menuArrow: e.detail.value
-    });
-  },
-  menuCard: function (e) {
-    this.setData({
-      menuCard: e.detail.value
-    });
-  },
-  switchSex: function (e) {
-    this.setData({
-      skin: e.detail.value
-    });
-  },
-
-  // ListTouch触摸开始
-  ListTouchStart(e) {
-    this.setData({
-      ListTouchStart: e.touches[0].pageX
-    })
-  },
-
-  // ListTouch计算方向
-  ListTouchMove(e) {
-    this.setData({
-      ListTouchDirection: e.touches[0].pageX - this.data.ListTouchStart > 0 ? 'right' : 'left'
-    })
-  },
-
-  // ListTouch计算滚动
-  ListTouchEnd(e) {
-    if (this.data.ListTouchDirection == 'left') {
-      this.setData({
-        modalName: e.currentTarget.dataset.target
-      })
-    } else {
-      this.setData({
-        modalName: null
-      })
-    }
-    this.setData({
-      ListTouchDirection: null
-    })
-  },
 })
