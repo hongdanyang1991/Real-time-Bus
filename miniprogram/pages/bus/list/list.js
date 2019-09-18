c7b3979c99796e2fd7db494470dafee08eda8cf9
hongdanyang <hongdanyang1991@163.com>
2019-09-18 15:30:12 +0800

    添加小程序云函数、云数据库

--- miniprogram/pages/bus/list/list.js
+++ miniprogram/pages/bus/list/list.js
@@ -1,5 +1,31 @@
 const app = getApp();
+
+
 Page({
+
+  onQuery: function () {
+    const db = wx.cloud.database()
+    // 查询当前用户所有的 counters
+    db.collection('counters').where({
+      _openid: this.data.openid
+    }).get({
+      success: res => {
+        this.setData({
+          queryResult: JSON.stringify(res.data, null, 2)
+        })
+        console.log('[数据库] [查询记录] 成功: ', res)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
+  },
+  
+
   data: {
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
