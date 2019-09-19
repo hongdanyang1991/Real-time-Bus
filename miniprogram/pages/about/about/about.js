cda36e50af02c15d265d2ef6464ca47a9314e87f
hongdanyang <hongdanyang1991@163.com>
2019-09-19 11:30:43 +0800

    添加地址选择测试

--- miniprogram/pages/about/about/about.js
+++ miniprogram/pages/about/about/about.js
@@ -1,14 +1,36 @@
+
 const app = getApp();
 Page({
   data: {
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
     ColorList: app.globalData.ColorList,
+    areaList: {},
+    loading: true,
+    value: 330302
   },
+
   onLoad: function () { },
   pageBack() {
     wx.navigateBack({
       delta: 1
     });
-  }
+  },
+
+  onShow() {
+    wx.request({
+      url: 'https://cashier.youzan.com/wsctrade/uic/address/getAllRegion.json',
+      success: response => {
+        this.setData({
+          loading: false,
+          areaList: response.data.data
+        });
+      }
+    });
+
+  
+
+
+
+  },
 });
